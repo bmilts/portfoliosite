85b5a46b52a6699a72fad0ba2dd543eb55b6c643
bmilts <brendan.milton1211@gmail.com>
2018-12-30 10:36:25 +0000

    Added % skill count up using owl carousel currently adding other skills and counter usings countUp.js, in addition completed icons and fonts using font-awesome

--- js/script.js
+++ js/script.js
@@ -9,29 +9,54 @@ $(document).ready(function() {
 
 	var typed = new Typed('.typed', {
 		strings: ["2:1 BSc Computer Science", "Python", "Unity Augmented Reality", "Android", "Blockchain", "IOS"],
-		typeSpeed: 70,
+		typeSpeed: 110,
 		loop: true,
 		startDelay: 1000,
 		showCursor: false
 	});
 	
-		$('.owl-carousel').owlCarousel({
+	$('.owl-carousel').owlCarousel({
 	    loop:true,
-	    margin:10,
-	    nav:true,
+	    items: 6,
 	    responsive:{
 	        0:{
 	            items:1
 	        },
-	        600:{
-	            items:3
+	        480:{
+	            items:2
 	        },
-	        1000:{
-	            items:5
+	        768:{
+	            items:4
+	        },
+	        938:{
+	            items:6
 	        }
 	    }
 	});
 	
+   
+    
+    // Get position of the element to offset
+    var skillsTopOffset = $(".skillSection").offset().top;
+    
+    // Window scroll event
+    $(window).scroll(function(){
+    	if(window.pageYOffset > skillsTopOffset - $(window).height() + 200){
+    		 $('.chart').easyPieChart({
+		        //your options goes here
+		        easing: 'easeInOut',
+		        barColor: '#fff',
+		        trackColor: false,
+		        scaleColor: false,
+		        lineWidth: 4,
+		        size: 152,
+		        onStep: function(from, to, percent){
+		        	$(this.el).find('.percent').text(Math.round(percent));
+		        }
+		        
+		  });
+    	}	
+    });
 	
 });
 
