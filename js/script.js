f5a6e53f2e38d1064987abbe16233630d694e231
bmilts <brendan.milton1211@gmail.com>
2019-01-02 13:49:56 +0000

    Added navbar, next step is to make it stick to page during scroll.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+//  $(window).on("load", function(){
+// 	$(".loader .inner").fadeOut(500, function(){
+// 		$(".loader").fadeOUt(750);
+// 	});
+
+// });
+
 $(document).ready(function() {
 
     // Jquery function for superslides   
